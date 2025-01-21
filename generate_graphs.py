05a91c5aeb19d8332ba14368e3cad9691f22245b
aetaffe <aetaffe@ucdavis.edu>
2025-01-20 21:10:04 -0800

    Added script to plot G/D loss

---
+++ generate_graphs.py
@@ -0,0 +1,41 @@
+import json
+import matplotlib.pyplot as plt
+
+
+def get_stats_dict(stats_path):
+    stats_dict = {}
+    with open(stats_path) as f:
+        lines = f.readlines()
+        for line in lines:
+            stat_line = json.loads(line)
+            for key, value in stat_line.items():
+                data = stats_dict.get(key, [])
+                data.append(value)
+                stats_dict[key] = data
+    return stats_dict
+
+
+if __name__ == '__main__':
+    stats_path = '/Users/alex/research/SyntheticData/stylegan3/stats_1.jsonl'
+    stats = get_stats_dict(stats_path)
+    loss_g_mean = []
+    loss_d_mean = []
+    k_imgs = []
+    for item in stats['Loss/G/loss']:
+        loss_g_mean.append(item['mean'])
+    for item in stats['Loss/D/loss']:
+        loss_d_mean.append(item['mean'])
+    for item in stats['Progress/kimg']:
+        k_imgs.append(item['mean'] + 900)
+    plt.plot(k_imgs, loss_g_mean, label='Generator Loss')
+    plt.plot(k_imgs, loss_d_mean, label='Discriminator Loss')
+    plt.xlabel('K Images')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.title('Loss vs K Images')
+    plt.savefig('loss_vs_kimgs_gamma_4.png')
+    plt.show()
+
+
+
+
